b35c83ef290c3c9745cea7dad1ef72a0498cb412
EMILY CREPALDI DE CARVALHO <23111719@CFP127ALUNO.LOCAL>
2023-09-15 16:53:38 -0300

    fazer!

---
+++ exercicio-01/app.js
@@ -0,0 +1,8 @@
+/************************************************************************
+ * Objetivo: Um consultório de Nutrição solicitou a você que criasse um sistema 
+ *           que faça o cálculo do IMC de uma pessoa
+ * Data: 15/09/2023
+ * Autor: Emily Crepaldi
+ * Versão: 1.0
+ ************************************************************************/
+
